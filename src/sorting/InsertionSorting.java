d051207fd240534ca9c02a9a93e4a16fa8c270fe
Oleg <unxxxt@gmail.com>
2024-12-24 01:28:37 +0300

    Add insertion sort logic

--- src/sorting/InsertionSorting.java
+++ src/sorting/InsertionSorting.java
@@ -3,9 +3,27 @@ package sorting;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+ * Реализация сортировки вставками.
+ */
 public class InsertionSorting<T> implements ISortingStrategy<T>{
+
     @Override
     public void sort(List<T> instances, Comparator<T> comparator) {
+        if (instances == null || instances.isEmpty()) {
+            return;
+        }
 
+        for (int i = 1; i < instances.size(); i++) {
+            T currentElement = instances.get(i);
+            int j = i - 1;
+
+            while (j >= 0 && comparator.compare(instances.get(j), currentElement) > 0) {
+                instances.set(j + 1, instances.get(j));
+                j--;
+            }
+
+            instances.set(j + 1, currentElement);
+        }
     }
 }
