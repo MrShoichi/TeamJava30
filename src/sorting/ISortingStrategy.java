ec48979c06d8ded7cf79b3b8ff837189e162c2f5
Oleg <unxxxt@gmail.com>
2024-12-24 01:23:30 +0300

    Add Javadoc to ISortingStrategy

--- src/sorting/ISortingStrategy.java
+++ src/sorting/ISortingStrategy.java
@@ -3,6 +3,19 @@ package sorting;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+ * Интерфейс стратегии сортировки.
+ * Определяет общий контракт для сортировок.
+ *
+ * @param <T> тип объектов, которые будут сортироваться.
+ */
 public interface ISortingStrategy<T> {
+
+    /**
+     * Сортирует список объектов с использованием компаратора.
+     *
+     * @param instances список объектов для сортировки.
+     * @param comparator компаратор, определяющий порядок сортировки объектов.
+     */
     void sort(List<T> instances, Comparator<T> comparator);
 }
